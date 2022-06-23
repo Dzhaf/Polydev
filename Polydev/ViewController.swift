0f5f2725c4ea9df5d7f64246b8d4ff8fb0e01408
Dzhafar <iamdzhafar@gmail.com>
2022-06-23 09:32:27 +0300

    Initial Commit

---
+++ Polydev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Polydev
+//
+//  Created by Jafar on 23.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
